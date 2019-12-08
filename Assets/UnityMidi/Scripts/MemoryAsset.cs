4b8ad2fd35907781b5046318df85d22f391889ad
Korobetski Sigfrid <lunaticchimera@gmail.com>
2019-12-09 00:10:01 +0100

    Create MemoryAsset.cs

---
+++ Assets/UnityMidi/Scripts/MemoryAsset.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.IO;
+using AudioSynthesis;
+
+namespace UnityMidi
+{
+    [System.Serializable]
+    public class MemoryAsset : IResource
+    {
+        private string _name;
+        private byte[] _datas;
+
+        public MemoryAsset(string name, byte[] datas)
+        {
+            _name = name;
+            _datas = datas;
+        }
+
+        public bool ReadAllowed()
+        {
+            return true;
+        }
+
+        public bool WriteAllowed()
+        {
+            return false;
+        }
+
+        public bool DeleteAllowed()
+        {
+            return false;
+        }
+
+        public string GetName()
+        {
+            return _name;
+        }
+
+        public Stream OpenResourceForRead()
+        {
+            return new MemoryStream(_datas);
+        }
+
+        public Stream OpenResourceForWrite()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void DeleteResource()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
